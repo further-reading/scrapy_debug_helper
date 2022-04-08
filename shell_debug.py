670d5a0cf2a260921d253deb971daec8ac561f1f
Roy <roy.healy87+gh@gmail.com>
2022-04-08 14:37:14 +0100

    Update shell_debug.py

--- shell_debug.py
+++ shell_debug.py
@@ -2,13 +2,12 @@ from scrapy import cmdline
 
 spider = 'spider_name_here'
 arguments = [
-    # 'name=value',
+    # '-a name=value',
 ]
 
 command = f'scrapy crawl {spider}'
 if arguments:
-    arguments = " -a ".join(arguments)
-    # -a will not be present for for first argument
-    command = f'{command} -a {arguments}'
+    arguments = " ".join(arguments)
+    command = f'{command} {arguments}'
 
 cmdline.execute(command.split())
